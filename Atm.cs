fdb98960ed206430fccc38222ea0dc4335a97f16
Gustavo Fiaschi <gustavo.fiaschi@clevertech.biz>
2023-12-13 17:22:07 -0300

    fix array payouts

--- Atm.cs
+++ Atm.cs
@@ -2,7 +2,7 @@ using System.Text;
 
 public static class Atm {
     private static int[] cartridges = [10, 50, 100];
-    internal static int[] payouts = [100];//[30, 50, 60, 80, 140, 230, 370, 610, 980];
+    internal static int[] payouts = [30, 50, 60, 80, 140, 230, 370, 610, 980];
 
     public static string GetCombinations(int option) {  
         StringBuilder sb = new StringBuilder();
